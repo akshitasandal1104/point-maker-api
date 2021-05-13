2d40ae3b680c38a5686cf7e51a37c90d3e532e96
Akshita Sandal <akshitasandal1104@gmail.com>
2021-05-13 23:21:37 +0530

    Update API integration

--- app/controllers/api/v1/points_controller.rb
+++ app/controllers/api/v1/points_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PointsController < ApplicationController
+    before_action :set_point, only: [:update]
+
     def index
         render json: Point.all
     end
@@ -12,9 +14,21 @@ class Api::V1::PointsController < ApplicationController
         end
     end
 
+    def update
+        if @point.update(point_params)
+            render json: { success: "Point has been updated successfully." }, status: :ok
+        else
+            render json: { errors: point.errors }, status: :unprocessable_entity
+        end
+    end
+
     private
 
     def point_params
         params.require(:point).permit(:title, :coordinates)
+    end
+
+    def set_point
+        @point = Point.where(id: params[:id]).first
     end
 end
