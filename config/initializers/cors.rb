6fc5c4ba61a307ef9926d0d72aca453754c98cca
Akshita Sandal <akshitasandal1104@gmail.com>
2021-05-13 23:09:58 +0530

    Updates.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://glacial-scrubland-15511.herokuapp.com/'
-  
+      origins '*'    
       resource '*',
         headers: :any,
-        methods: [:get, :post]
+        methods: [:get, :post, :put, :patch, :delete, :options, :head, :others]
     end
   end
+  
