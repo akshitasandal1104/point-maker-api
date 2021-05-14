aa0afdc598f20215aea2a1081d1548c7f3254e30
Akshita Sandal <akshitasandal1104@gmail.com>
2021-05-14 22:30:04 +0530

    Responsive fixes and COntact page fix.

--- app/controllers/api/v1/points_controller.rb
+++ app/controllers/api/v1/points_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PointsController < ApplicationController
     before_action :set_point, only: [:update, :destroy]
-    skip_before_filter :verify_authenticity_token , only: [:create]
+    skip_before_action :verify_authenticity_token , only: [:create]
 
     def index
         render json: Point.all
